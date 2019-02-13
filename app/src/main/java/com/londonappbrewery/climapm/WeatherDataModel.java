d3428cdf7f2574cb345f2b7d3a719d457effbcd1
pankaj <pankajmulchandani08@gmail.com>
2019-02-13 16:34:36 +0530

    Completed App

--- app/src/main/java/com/londonappbrewery/climapm/WeatherDataModel.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherDataModel.java
@@ -1,47 +1,87 @@
 package com.londonappbrewery.climapm;
 
+import android.util.JsonReader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class WeatherDataModel {
 
     // TODO: Declare the member variables here
+    String Temperature;
+    String mCity;
+    String mIconName;
+    int mCondition;
+
 
 
     // TODO: Create a WeatherDataModel from a JSON:
 
+    public static WeatherDataModel fromJson(JSONObject jsonObject){
+        try {
 
-    // TODO: Uncomment to this to get the weather image name from the condition:
-//    private static String updateWeatherIcon(int condition) {
-//
-//        if (condition >= 0 && condition < 300) {
-//            return "tstorm1";
-//        } else if (condition >= 300 && condition < 500) {
-//            return "light_rain";
-//        } else if (condition >= 500 && condition < 600) {
-//            return "shower3";
-//        } else if (condition >= 600 && condition <= 700) {
-//            return "snow4";
-//        } else if (condition >= 701 && condition <= 771) {
-//            return "fog";
-//        } else if (condition >= 772 && condition < 800) {
-//            return "tstorm3";
-//        } else if (condition == 800) {
-//            return "sunny";
-//        } else if (condition >= 801 && condition <= 804) {
-//            return "cloudy2";
-//        } else if (condition >= 900 && condition <= 902) {
-//            return "tstorm3";
-//        } else if (condition == 903) {
-//            return "snow5";
-//        } else if (condition == 904) {
-//            return "sunny";
-//        } else if (condition >= 905 && condition <= 1000) {
-//            return "tstorm3";
-//        }
-//
-//        return "dunno";
-//    }
+            WeatherDataModel weatherDataModel = new WeatherDataModel();
+
+            weatherDataModel.mCity = jsonObject.getString("name");
+            weatherDataModel.mCondition = jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
+            weatherDataModel.mIconName = updateWeatherIcon(weatherDataModel.mCondition);
+
+            double tempResult =jsonObject.getJSONObject("main").getDouble("temp") -273.15;
+            int roundedValue = (int ) Math.rint(tempResult);
+            weatherDataModel.Temperature= Integer.toString(roundedValue);
+
+            return weatherDataModel;
+        }catch(JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+  //   TODO: Uncomment to this to get the weather image name from the condition:
+    private static String updateWeatherIcon(int condition) {
+
+        if (condition >= 0 && condition < 300) {
+            return "tstorm1";
+        } else if (condition >= 300 && condition < 500) {
+            return "light_rain";
+        } else if (condition >= 500 && condition < 600) {
+            return "shower3";
+        } else if (condition >= 600 && condition <= 700) {
+            return "snow4";
+        } else if (condition >= 701 && condition <= 771) {
+            return "fog";
+        } else if (condition >= 772 && condition < 800) {
+            return "tstorm3";
+        } else if (condition == 800) {
+            return "sunny";
+        } else if (condition >= 801 && condition <= 804) {
+            return "cloudy2";
+        } else if (condition >= 900 && condition <= 902) {
+            return "tstorm3";
+        } else if (condition == 903) {
+            return "snow5";
+        } else if (condition == 904) {
+            return "sunny";
+        } else if (condition >= 905 && condition <= 1000) {
+            return "tstorm3";
+        }
+
+        return "dunno";
+    }
 
     // TODO: Create getter methods for temperature, city, and icon name:
 
 
+    public String getTemperature() {
+        return Temperature;
+    }
+
+    public String getmCity() {
+        return mCity;
+    }
+
+    public String getmIconName() {
+        return mIconName;
+    }
 
 }
